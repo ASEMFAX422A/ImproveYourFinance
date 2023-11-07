a6a521c4999cf367ed2bdc46580bb794f61bae99
Keule0010 <pk@pkcore.de>
2023-11-07 03:05:33 +0100

    Fixed test

--- backend/src/test/java/org/pdf/finanzverwaltung/RegistrationControllerTest.java
+++ backend/src/test/java/org/pdf/finanzverwaltung/RegistrationControllerTest.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.pdf.finanzverwaltung.registration.*;
+import org.pdf.finanzverwaltung.controllers.RegistrationController;
+import org.pdf.finanzverwaltung.controllers.RegistrationController.Response;
+import org.pdf.finanzverwaltung.models.RegistrationRequest;
+import org.pdf.finanzverwaltung.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RegistrationControllerTest {
@@ -18,33 +21,31 @@ public class RegistrationControllerTest {
     private RegistrationController registrationController;
 
     @Mock
-    private RegistrationService registrationService;
+    private UserService userService;
 
     @Test
     public void testRegistrationSuccess() {
         // Arrange
         RegistrationRequest request = new RegistrationRequest("validUsername", "validPassword");
-        when(registrationService.register(request)).thenReturn(true);
+        when(userService.addUser(any())).thenReturn(true);
 
         // Act
-        ResponseEntity<String> response = registrationController.register(request);
+        ResponseEntity<Response> response = registrationController.register(request);
 
         // Assert
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("user registered", response.getBody());
     }
 
     @Test
     public void testRegistrationFailure() {
         // Arrange
         RegistrationRequest request = new RegistrationRequest("invalidUsername", "invalidPassword");
-        when(registrationService.register(request)).thenReturn(false);
+        when(userService.addUser(any())).thenReturn(false);
 
         // Act
-        ResponseEntity<String> response = registrationController.register(request);
+        ResponseEntity<Response> response = registrationController.register(request);
 
         // Assert
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals("user could not be registered", response.getBody());
     }
 }
