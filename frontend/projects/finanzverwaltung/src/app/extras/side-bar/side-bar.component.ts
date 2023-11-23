f83a1d7b287fe39e5c6e65526a6ffcb215a9850f
Ffrankys <FfrankysZockt@gmail.com>
2023-11-23 04:15:57 +0100

    use routerLink instead of href

--- frontend/projects/finanzverwaltung/src/app/extras/side-bar/side-bar.component.ts
+++ frontend/projects/finanzverwaltung/src/app/extras/side-bar/side-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone:true,
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.scss']
+  styleUrls: ['./side-bar.component.scss'],
+  imports: [RouterLink]
 })
 export class SideBarComponent {
 
